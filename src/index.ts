c358022f382af7afc8f0fbbfa4422fcc2a419f1b
Kengo TODA <skypencil@gmail.com>
2020-02-07 15:33:30 +0800

    docs: create documents for users

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import * as props from "properties"
 
+/**
+ * Parse the file pointed by the given path as [the Properties defined by Java](https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/Properties.html).
+ *
+ * @param path - Path to the target file to parse
+ * @returns Promise which returns parsed properties
+ */
 export async function parseFile(path: string): Promise<object> {
   return new Promise((resolve, reject) => {
     props.parse(path, { path: true }, (err: Error, result: object) => {
@@ -11,10 +17,18 @@ export async function parseFile(path: string): Promise<object> {
     })
   })
 }
+
 function objToMap<V>(obj: { [key: string]: string }): Map<string, string> {
     return new Map(Object.entries(obj));
 }
-export async function parse(data: string): Promise<Map<string,string>> {
+
+/**
+ * Parse given text as [the Properties defined by Java](https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/Properties.html).
+ *
+ * @param data - Text to parse
+ * @returns Promise which returns parsed properties
+ */
+export async function parse(data: string): Promise<Map<string, string>> {
   return new Promise((resolve, reject) => {
     props.parse(data, { path: false }, (err: Error, result: { [key: string]: string }) => {
       if (err) {
@@ -25,10 +39,23 @@ export async function parse(data: string): Promise<Map<string,string>> {
     })
   })
 }
+
+/**
+ * Convert properties data to text, based on [the spec defined by Java](https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/Properties.html).
+ * @param data - Properties data to convert into text format
+ * @returns Converted text which represents the given properties
+ */
 export async function stringify(data: object): Promise<string> {
   const result = props.stringify(data, { unicode: true }) as string
   return Promise.resolve(result)
 }
+/**
+ * Convert properties data to text, based on [the spec defined by Java](https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/Properties.html),
+ * and write it to the file pointed by the given path.
+ * @param data - Properties data to convert into text format
+ * @param path - Path to the target file to write
+ * @returns Promise which is resolved when file is successfully written
+ */
 export async function write(data: object, path: string): Promise<undefined> {
   return new Promise((resolve, reject) => {
     props.stringify(data, { path }, (err: Error) => {
