c802ff4d6ce3f55b631d545656e314fb3e23d459
Kengo TODA <skypencil@gmail.com>
2020-02-07 20:31:09 +0800

    fix: return Map instead of object by #parseFile()

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -2,27 +2,114 @@ import "mocha"
 import { expect } from "chai"
 import { promises } from "fs"
 import { file } from "tmp-promise";
-import { parse, stringify, write } from "../src/index"
+import { parse, parseFile, stringify, write } from "../src/index"
 
 describe("public API", () => {
-  it("provides methods returning Promise instance", () => {
-    const result = stringify({ foo: "bar" })
-    expect(result).to.be.an.instanceOf(Promise)
+  describe("#parseFile", () => {
+    it("parses .properties file", async () => {
+      return file().then(async fileResult => {
+        await promises.writeFile(fileResult.path, "foo=bar")
+        const result = await parseFile(fileResult.path)
+        expect(result.get('foo')).to.equal('bar')
+      })
+    })
+    it("uses colon as key terminator", async () => {
+      return file().then(async fileResult => {
+        await promises.writeFile(fileResult.path, "foo:bar")
+        const result = await parseFile(fileResult.path)
+        expect(result.get('foo')).to.equal('bar')
+      })
+    })
+    it("parses multiple natural lines in one logical line", async () => {
+      return file().then(async fileResult => {
+        await promises.writeFile(fileResult.path, 'foo=bar\\\nbaz')
+        const result = await parseFile(fileResult.path)
+        expect(result.get('foo')).to.equal('barbaz')
+      })
+    })
+    it("ignore spaces at the head of line", async () => {
+      return file().then(async fileResult => {
+        await promises.writeFile(fileResult.path, '    foo=bar')
+        const result = await parseFile(fileResult.path)
+        expect(result.get('foo')).to.equal('bar')
+      })
+    })
+    it("ignore spaces before key terminator", async () => {
+      return file().then(async fileResult => {
+        await promises.writeFile(fileResult.path, 'foo    =bar')
+        const result = await parseFile(fileResult.path)
+        expect(result.get('foo')).to.equal('bar')
+      })
+    })
+    it("ignore spaces after key terminator", async () => {
+      return file().then(async fileResult => {
+        await promises.writeFile(fileResult.path, 'foo=    bar')
+        const result = await parseFile(fileResult.path)
+        expect(result.get('foo')).to.equal('bar')
+      })
+    })
+    it("escapes key terminator by back-slash", async () => {
+      return file().then(async fileResult => {
+        await promises.writeFile(fileResult.path, 'foo\\=bar=baz')
+        const result = await parseFile(fileResult.path)
+        expect(result.get('foo=bar')).to.equal('baz')
+      })
+    })
+    it("ignores comment", async () => {
+      return file().then(async fileResult => {
+        await promises.writeFile(fileResult.path, '# this is comment\r\nfoo=bar')
+        const result = await parseFile(fileResult.path)
+        expect(result.get('foo')).to.equal('bar')
+        expect(result).to.have.lengthOf(1)
+      })
+    })
+    xit("set an empty string if there is no non-whitespace char after the key terminator", async () => {
+      return file().then(async fileResult => {
+        await promises.writeFile(fileResult.path, 'foo=\nbar= ')
+        const result = await parseFile(fileResult.path)
+        expect(result.get('foo')).to.equal('')
+        expect(result.get('bar')).to.equal('')
+      })
+    })
+    xit("set an empty string if there is no key terminator", async () => {
+      return file().then(async fileResult => {
+        await promises.writeFile(fileResult.path, 'foo')
+        const result = await parseFile(fileResult.path)
+        expect(result.get('foo')).to.equal('')
+      })
+    })
   })
-  it("stringify properties", (done) => {
-    stringify({ foo: "bar", baz: 42 }).then(result => {
-      expect(result).to.include("foo = bar")
-      expect(result).to.include("baz = 42")
-      done()
+  describe("#stringify", () => {
+    it("provides methods returning Promise instance", () => {
+      const result = stringify({ foo: "bar" })
+      expect(result).to.be.an.instanceOf(Promise)
+    })
+    it("stringify properties", async () => {
+      return stringify({ foo: "bar", baz: 42 }).then(result => {
+        expect(result).to.include("foo = bar")
+        expect(result).to.include("baz = 42")
+      })
+    })
+    it("escapes multibyte chars", async () => {
+      const result = await stringify({ face: "😁" })
+      expect(result).to.include("face = \\ud83d\\ude01")
+    })
+    it("escapes CR", async () => {
+      const result = await stringify({ text: "foo\rbar" })
+      expect(result).to.include("text = foo\\rbar")
+    })
+    it("escapes LF", async () => {
+      const result = await stringify({ text: "foo\nbar" })
+      expect(result).to.include("text = foo\\nbar")
+    })
+    it("escapes = in key", async () => {
+      const result = await stringify({ "foo=bar": "baz" })
+      expect(result).to.include("foo\\=bar = baz")
     })
   })
   it("parses escaped multibyte chars", async () => {
     const result = await parse("face = \\ud83d\\ude01")
     expect(result.get('face')).to.equal("😁")
-  })
-  it("escape multibyte chars", async () => {
-    const result = await stringify({ face: "😁" })
-    expect(result).to.include("face = \\ud83d\\ude01")
   })
   it("generates .properties file", (done) => {
     file().then(fileResult => {
