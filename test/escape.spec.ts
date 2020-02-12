189932000f60b6823f52b7cdd26bcd99b682e1db
Kengo TODA <skypencil@gmail.com>
2020-02-12 08:04:13 +0800

    feat: Implement stringify without properties module (#4)
    * feat: implement escape functions
    
    * feat: use escape funcs
    
    * test: make sure only given properties are stringified
    
    * docs: add TSDoc

---
+++ test/escape.spec.ts
@@ -0,0 +1,37 @@
+import "mocha"
+import { expect } from "chai"
+import { escape, escapeKey } from "../src/escape"
+
+
+describe("#escape", () => {
+    it("does not change empty string", () => {
+        expect(escape("")).to.equal("")
+    })
+    it("does not escapes =", () => {
+        expect(escape("foo=bar")).to.equal("foo=bar")
+    })
+    it("escapes emoticon", () => {
+        expect(escape("😁")).to.equal("\\ud83d\\ude01")
+    })
+    it("escapes CR", () => {
+        expect(escape("foo\rbar")).to.equal("foo\\rbar")
+    })
+    it("escapes LF", () => {
+        expect(escape("foo\nbar")).to.equal("foo\\nbar")
+    })
+    it("does not escape single/double quote", () => {
+        expect(escape("\'\"")).to.equal("\'\"")
+    })
+})
+
+describe("#escapeKey", () => {
+    it("escapes =", () => {
+        expect(escapeKey("foo=bar")).to.equal("foo\\=bar")
+    })
+    it("escapes :", () => {
+        expect(escapeKey("foo:bar")).to.equal("foo\\:bar")
+    })
+    it("escapes multiple key separators", () => {
+        expect(escapeKey("foo:bar=baz:last")).to.equal("foo\\:bar\\=baz\\:last")
+    })
+})
