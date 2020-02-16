5ea2ec4b7a7783f19060d6e3e8b2240b76260c97
Kengo TODA <skypencil@gmail.com>
2020-02-16 18:27:25 +0800

    fix: could not support number in key and value

--- src/parser.ts
+++ src/parser.ts
@@ -86,12 +86,12 @@ export const PropertiesParser: Parsimmon.Language = Parsimmon.createLanguage({
     return Parsimmon.oneOf(":=");
   },
   Key: (r: Parsimmon.Language) => {
-    return Parsimmon.regexp(/([a-zA-Z_.-]|\\u[a-z0-9]{4}|\\[\\a-zA-Z=:])+/)
+    return Parsimmon.regexp(/([a-zA-Z0-9_.-]|\\u[a-z0-9]{4}|\\[\\a-zA-Z0-9=:])+/)
       .trim(r.WhiteSpace)
       .map(interpretEscapes);
   },
   Value: (r: Parsimmon.Language) => {
-    return Parsimmon.regexp(/([a-zA-Z_.-]|\\u[a-z0-9]{4}|\\[\\a-zA-Z=:])+/)
+    return Parsimmon.regexp(/([a-zA-Z0-9_.-]|\\u[a-z0-9]{4}|\\[\\a-zA-Z0-9=:])+/)
       .trim(r.WhiteSpace)
       .map(interpretEscapes);
   }
